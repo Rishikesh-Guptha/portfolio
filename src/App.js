c41b8df06d5450a647b0a73fd8d46b22f7d72cc4
Rishikesh-Guptha <rishikeshrishi2004@gmail.com>
2023-10-25 23:28:42 +0530

    Created a new react app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 /** @format */
 
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./pages/Navbar";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 
 function App() {
-	return <div className="App">hello</div>;
+	return (
+		<div className="App">
+			<Navbar />
+			<BrowserRouter>
+				<Routes>
+					<Route
+						path="/"
+						element={<Home />}
+					/>
+					<Route
+						path="/projects"
+						element={<Projects />}
+					/>
+					<Route
+						path="/contact"
+						element={<Contact />}
+					/>
+				</Routes>
+			</BrowserRouter>
+			<Projects />
+			<Contact />
+		</div>
+	);
 }
 
 export default App;
--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,9 +1,40 @@
 /** @format */
 
 import React from "react";
+import "./Navbar.css";
+import { BrowserRouter, Link, Outlet } from "react-router-dom";
 
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<div className="navbar">
+			<BrowserRouter>
+				<div className="navbar__logo">RKG</div>
+				<div className="navbar__links">
+					<div className="links">
+						<Link
+							to="/"
+							style={{ textDecoration: "none", color: "black" }}>
+							Home
+						</Link>
+					</div>
+					<div className="links">
+						<Link
+							to="/Projects"
+							style={{ textDecoration: "none", color: "black" }}>
+							Projects
+						</Link>
+					</div>
+					<div className="links">
+						<Link
+							to="/Contact"
+							style={{ textDecoration: "none", color: "black" }}>
+							Contact Me
+						</Link>
+					</div>
+				</div>
+			</BrowserRouter>
+		</div>
+	);
 };
 
 export default Navbar;
